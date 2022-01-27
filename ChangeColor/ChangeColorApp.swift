f09102b4a852e3efbac423e3580a30d838907758
tipachel88 <tipakoroche@gmail.com>
2022-01-27 17:51:57 +0300

    Initial Commit

---
+++ ChangeColor/ChangeColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorApp.swift
+//  ChangeColor
+//
+//  Created by Tipachel on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
